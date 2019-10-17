455484d768d3952a1fd0be171a3659d7612f1e79
KewinLizarraga <kewin392@gmail.com>
2019-10-17 17:34:56 -0500

    add user data

--- db.js
+++ db.js
@@ -3,17 +3,25 @@ const products = [
     id: '91c0emq9wqm0e9',
     name: 'Media',
     price: 50.00,
-    currency: 'PEN'
+    currency: 'PEN',
+    owner: '91c0emq9wq34o0'       // ID del user
   },
   {
     id: 'as09as9d0a9d0-a9',
     name: 'Pantalon',
     price: 150.00,
-    currency: 'PEN'
+    currency: 'PEN',
+    owner: '91c0emq9wq34o0'       // ID del user
   },
 ]
 
-const users = [];
+const users = [
+  {
+    id: '91c0emq9wq34o0',
+    name: 'Kewin',
+    nickname: 'krlm',
+  }
+];
 
 // Trailing comma
 module.exports = {
