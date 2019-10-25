85cbe843254f6df646dfdf1a4a0c3c45cce13b98
KewinLizarraga <kewin392@gmail.com>
2019-10-25 01:01:12 -0500

    add logger in the endpoints

--- resources/productos/products.routes.js
+++ resources/productos/products.routes.js
@@ -3,19 +3,24 @@ const uuidv4 = require('uuid/v4');
 
 const auth = require('../middlewares/auth');
 const validateProduct = require('./products.validate');
-
 let products = require('../../db').products;
+const logger = require('../lib/logger');
 
 const productsRoutes = express.Router()
 
 productsRoutes.get('/', auth, (req, res) => {
+  logger.info('Se obtuvo todos los productos');
   res.json(products);
 });
 
 productsRoutes.post('/', auth, validateProduct, (req, res) => {
   const newProduct = { ...req.body, id: uuidv4() };
-  products.push(newProduct);
-  res.json(newProduct);
+  try {
+    products.push(newProduct);
+    res.json(newProduct);
+  } catch (error) {
+    logger.error('No se pudo optener la DB');
+  }
 })
 
 ///products/098as908asd098asd089
@@ -24,6 +29,7 @@ productsRoutes.put('/:id', auth, (req, res) => {
 
   if (filterProduct.owner === req.body.owner) {
     const updatedProduct = { ...filterProduct, ...req.body };
+    logger.info('Se actualizo con exito el producto');
     res.json(updatedProduct);
   } else {
     return res.status(403).send('No tienes permiso.');
--- resources/users/users.routes.js
+++ resources/users/users.routes.js
@@ -1,29 +1,34 @@
 const express = require('express');
 const uuidv4 = require('uuid');
-
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const validateUsers = require('./users.validate');
-
 let users = require('../../db').users;
+const logger = require('../lib/logger');
 
 const usersRoutes = express.Router();
 
 usersRoutes.get('/', (req, res) => {
+	logger.info('Se obtuvo los usuarios');
 	res.json(users);
 });
 
 usersRoutes.post('/', validateUsers, (req, res) => {
 	const hp = bcrypt.hashSync(req.body.password, 10);
 	const newUser = { ...req.body, id: uuidv4(), password: hp };
-	users.push(newUser);
-	res.json(newUser);
+	try {
+		users.push(newUser);
+		res.json(newUser);
+	} catch (error) {
+		logger.error('No se pudo obtener la DB');
+	}
 });
 
 usersRoutes.put('/:id', (req, res) => {
 	const filterUser = users.filter(user => user.id === req.params.id)[0];
 	const updatedUser = { ...filterUser, ...req.body };
+	logger.info('Se actualizo con exito al usuario');
 	res.json(updatedUser);
 });
 
@@ -39,12 +44,10 @@ usersRoutes.post('/login', (req, res) => {
 	const password = req.body.password;
 
 	const user = users.filter(user => user.username === username)[0];
-
 	const isAuthenticated = bcrypt.compareSync(password, user.password);
 
 	if (isAuthenticated) {
 		const token = jwt.sign({ id: user.id }, 'SECRET_KEY', { expiresIn: '10h' });
-
 		res.json({ token });
 	} else {
 		res.status(401).send('Verifica tu password');
